dd8e966323bac544981806a252a18d547908ab1f
E7ite <z7z.e7ite.z7z@gmail.com>
2021-06-25 20:00:37 -0700

    BIOS Disk CHS read example implemented

--- boot.asm
+++ boot.asm
@@ -11,15 +11,6 @@
 ; 0x00-0x400: Interrupt vector table (256 entries of 4 bytes)
 ; 0x7C00: Our bootloader
 
-; BIOS Video subroutine Int 10
-; Displays character in AL to screen, advances cursor, and scrolls screen if needed
-; AX:
-;    AH: 0Eh (teletype output)
-;    AL: character to write
-; BX:
-;   BH: Page number
-;   BL: foreground color (graphics mode)
-
 ; Tell NASM that we expect to be loaded at offset 0 in RAM. In actuality, we 
 ; will be loaded in 0x7C00 in RAM by the BIOS. But every data access will be
 ; offset by 0x7C0 * 0x10 due to the segmentation since we setup the registers up
@@ -44,13 +35,6 @@ bpb_start:
 set_csip:
     jmp 7C0h:start
 
-; My first interrupt implementation which should be int 0. The processor
-; expects interrupt 0 to handle divide by zero, so this should do so
-div_by_zero:
-    mov si, div_zero_err
-    call print
-    iret
-
 ; Entry point
 start:
     ; Disable interrupts by clearing interrupt flag to change segment registers.
@@ -69,26 +53,52 @@ start:
     mov ss, ax     ; Stack segment
     mov sp, 07C00h ; Stack pointer, will grow downwards
     ; Enables interrupts again by setting the interrupt flags
-    sti 
+    sti
 
-    ; Set the first interrupt's offset to point to the function div_by_zero
-    ; in the interrupt vector table to invoke the div_by_zero subroutine.
-    ; Using the stack segment since we do not want to implicitly use the data
-    ; segment since it is 0x7C0
-    mov word [ss:0], div_by_zero
-    ; Set the first interrupt's segment to aid in pointing to where our bootloader is
-    mov word [ss:2], 7C0h
-
-    ; Print alert of the message
-    mov si, stuff
+    ; BIOS Disk subroutine Int 13h 
+    ; AX:
+    ;   AH: 02h Read Sector Using CHS (cylinder head sector) Will read a sector from specified disk
+    ;           and store that sector into buffer pointed to by ES:BX
+    ;   AL: Number of sectors to read (cannot be zero)
+    ; CX:
+    ;   CH: Low eight bits of the cylinder number
+    ;   CL: sector number 1-63 (bits 0-5). high two bits of cylinder (bits 6-7, hard disk only)
+    ; DX:
+    ;   DH: head number
+    ;   DL: Drive number
+    ; ES:BX: Data buffer to store sector to
+    ; 
+    ; Returns
+    ; CF flag set if error. Cleareed if successful
+    ; If AH == 11h (Corrected ECC error)
+    ;   AL = burst length
+    ; AH = status
+    ; AL = number of sectors transferred. Only valid if CF is set on some BIOSes)
+    ;
+    ; It is important to know that my BIOS might have some issues due to USB
+    ; emulation. THe cylinder head, and track numbers might be off.
+    ;
+    ; We didn't set DL before any point in this code, which is important because
+    ; the BIOS should have set DL to the drive number for us. Later we should
+    ; preserve it on the stack.
+    mov ah, 2      ; Disk read mode
+    mov al, 1      ; Number of sectors to read
+    mov ch, 0      ; Low eight bits of the cylinder number
+    mov cl, 2      ; Sector 2 from hard disk.
+    mov dh, 0      ; Head number
+    mov bx, buffer ; THe buffer which will be accessed as ES:BX 
+    int 13h
+    jnc read_success ; If carry bit is not set, it was successful
+    ; If int 13h set the carry flag, we want to print error
+    mov si, disk_read_failure
     call print
-
-    ; Load the address of greeting string to si register
-    mov si, greeting
-
-    ; Call our subroutine to print the string
+    jmp done 
+read_success:
+    ; Print the data buffer. We zeroed out the sector so we are know there is
+    ; a spot to stop reading the string from
+    mov si, buffer
     call print
-
+done:
     ; Jump to itself so we don't try to execute anything afterwards
     jmp $
 
@@ -116,7 +126,16 @@ end:
     ret
 
 ; print: Prints a single character using the BIOS video subroutine
-; @param dl: the character to print
+; @param dl: the character to print.
+;
+; Uses BIOS Video subroutine Int 10h teletype output mode
+; AX:
+;    AH: 0Eh (teletype output) Displays character in AL to screen, advances cursor,
+;            and scrolls screen if needed
+;    AL: character to write
+; BX:
+;   BH: Page number
+;   BL: foreground color (graphics mode)
 printChar:
     ; Set to teletype output mode
     mov ah, 0Eh
@@ -128,10 +147,7 @@ printChar:
     int 10h
     ret
 
-; Greeting null-terminated string. 
-stuff db "About to divide by zero", 0Dh, 0Ah, 0
-greeting db "Hello world!", 0Dh, 0Ah, 0
-div_zero_err db "DivByZero", 0Dh, 0Ah, 0
+disk_read_failure: db "Failed to read hard disk", 0Dh, 0Ah, 0
 
 ; Pads the rest of the sector up to the 510th byte with 0s
 ; $: current address of program after adding everything above.
@@ -143,3 +159,8 @@ times 1FEh - ($ - $$) db 0
 ; we are a bootloader. Word at 0x511 should be 0x55AA (this CPU is little endian, 
 ; so should be 0xAA55)
 dw 0AA55h
+
+; We will have this label point to the physical address in RAM right after the 
+; bootloader after as a data buffer. This is where we will store the copy of the 
+; second hard disk sector which contains the message.
+buffer:
